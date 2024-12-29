e62fb3e6fedec18d5d31a5afa5275f488e0f9fdc
Cachumbin <simoncarreno2006@gmail.com>
2024-12-29 00:56:57 -0500

    feat: Add fileUpload basic functionality

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+
 const app = express();
 
 const PORT = 3000;
 
-app.use(express.json());
+app.use(fileUpload());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    console.log(req.files);
+
+    res.send('File uploaded!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
