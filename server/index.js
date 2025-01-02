b8112667b01bc337a4937ffcdfcee41abc8b9eb0
Cachumbin <simoncarreno2006@gmail.com>
2025-01-01 20:46:45 -0500

    fix: Enable dynamic routing from root directory

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.use(fileUpload());
 
 app.get('/', (req, res) => res.send('CachunHomeCloud'));
 
-app.post('/upload/:path', (req, res) => {
-    const requestedPath = req.params.path === 'root' ? '' : req.params.path.replace(/~/g, path.sep);
+app.post('/:path', (req, res) => {
+    const requestedPath = req.params.path.replace(/~/g, path.sep).replace('root', '');
     const uploadFolder = path.join(uploadsDir, requestedPath);
 
     if (!fs.existsSync(uploadFolder)) {
@@ -46,7 +46,7 @@ app.post('/upload/:path', (req, res) => {
 app.use('/uploads', express.static(uploadsDir));
 
 app.get('/:path', (req, res) => {
-    const requestedPath = req.params.path === 'root' ? '' : req.params.path.replace(/~/g, path.sep);
+    const requestedPath = req.params.path.replace(/~/g, path.sep).replace('root', '');
     const absolutePath = path.join(uploadsDir, requestedPath);
 
     if (fs.existsSync(absolutePath) && fs.lstatSync(absolutePath).isDirectory()) {
